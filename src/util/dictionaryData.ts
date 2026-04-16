52fe5b29ab39e5ed039935994edb82497fbbaa96
Ken_Cir <65713982+KenCir@users.noreply.github.com>
2026-04-16 21:58:31 +0900

    refactor: private修飾子に統一

--- src/util/dictionaryData.ts
+++ src/util/dictionaryData.ts
@@ -26,14 +26,14 @@ export const DictionaryDataSchema = z.object({
 export type DictionaryData = z.infer<typeof DictionaryDataSchema>;
 
 export class Dictionary {
-	readonly #guildId: string;
+	private readonly guildId: string;
 
-	#dictionaryData!: DictionaryData;
+	private _dictionaryData!: DictionaryData;
 
 	public constructor(guildId: string) {
-		this.#guildId = guildId;
-		this.#dictionaryData = {
-			id: this.#guildId,
+		this.guildId = guildId;
+		this._dictionaryData = {
+			id: this.guildId,
 			data: {},
 		};
 	}
@@ -41,31 +41,31 @@ export class Dictionary {
 	public async load(): Promise<void> {
 		try {
 			await access(
-				new URL(`../../data/dictionary/${this.#guildId}.json`, import.meta.url),
+				new URL(`../../data/dictionary/${this.guildId}.json`, import.meta.url),
 				constants.F_OK | constants.R_OK | constants.W_OK,
 			);
 		} catch {
 			return;
 		}
 
-		const rawData = await readFile(new URL(`../../data/dictionary/${this.#guildId}.json`, import.meta.url), 'utf8');
+		const rawData = await readFile(new URL(`../../data/dictionary/${this.guildId}.json`, import.meta.url), 'utf8');
 		try {
-			this.#dictionaryData = DictionaryDataSchema.parse(JSON.parse(rawData));
+			this._dictionaryData = DictionaryDataSchema.parse(JSON.parse(rawData));
 		} catch (error) {
-			throw new Error(`Failed to parse dictionary data for guild ${this.#guildId}: ${error}`);
+			throw new Error(`Failed to parse dictionary data for guild ${this.guildId}: ${error}`);
 		}
 	}
 
 	public async save(): Promise<void> {
 		await writeFile(
-			new URL(`../../data/dictionary/${this.#guildId}.json`, import.meta.url),
-			JSON.stringify(this.#dictionaryData),
+			new URL(`../../data/dictionary/${this.guildId}.json`, import.meta.url),
+			JSON.stringify(this._dictionaryData),
 			'utf8',
 		);
 	}
 
 	public upsert(word: string, replaceWord: string, options?: { flags?: string; isRegExp?: boolean }): void {
-		this.#dictionaryData.data[word] = {
+		this._dictionaryData.data[word] = {
 			replace: replaceWord,
 			flags: options?.flags ?? 'gi',
 			isRegExp: options?.isRegExp ?? false,
@@ -73,13 +73,13 @@ export class Dictionary {
 	}
 
 	public delete(word: string): boolean {
-		if (!this.#dictionaryData.data[word]) return false;
-		const { [word]: _, ...rest } = this.#dictionaryData.data;
-		this.#dictionaryData.data = rest;
+		if (!this._dictionaryData.data[word]) return false;
+		const { [word]: _, ...rest } = this._dictionaryData.data;
+		this._dictionaryData.data = rest;
 		return true;
 	}
 
 	public get dictionaryData(): DictionaryData {
-		return this.#dictionaryData;
+		return this._dictionaryData;
 	}
 }
